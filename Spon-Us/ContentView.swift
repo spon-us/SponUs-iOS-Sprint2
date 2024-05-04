8caad5afefdbedfea6a7e2851667da0e181ffb5a
00yhsp <00yhsp@naver.com>
2024-05-04 14:47:35 +0900

    add gitignore

---
+++ Spon-Us/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spon-Us
+//
+//  Created by 박현수 on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spon-Us/Spon_UsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spon_UsApp.swift
+//  Spon-Us
+//
+//  Created by 박현수 on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Spon_UsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
