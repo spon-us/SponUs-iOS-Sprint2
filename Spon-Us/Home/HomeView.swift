eda6d7412d97012e4b8ce55d1dab953f8c8e5e06
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2024-05-14 11:45:22 +0900

    #9 home view UI (#10)
    * 홈뷰 짜는중
    
    * #9-HomeView-UI

--- Spon-Us/Home/HomeView.swift
+++ Spon-Us/Home/HomeView.swift
@@ -12,18 +12,19 @@ struct HomeView: View {
     @State var homeViewModel = HomeViewModel()
     var body: some View {
         VStack(spacing: 0) {
-            StatusBarView(homeViewModel: homeViewModel)
-            ScrollView {
-                if homeViewModel.isPortfolioUploaded {
-                    CardGuestView(homeViewModel: homeViewModel)
-                }
-            }.padding(.horizontal, 20)
+            HomeStatusBarView(homeViewModel: homeViewModel)
+            if homeViewModel.isPortfolioUploaded {
+                HomeCardGuestView(homeViewModel: homeViewModel)
+            }
+            HomeSelectionTabView(homeViewModel: homeViewModel)
+            HomeCategorySelectionTab(homeViewModel: homeViewModel)
+            HomeListView(homeViewModel: homeViewModel)
         }.background(Color.bgSecondary)
     }
 }
 
 
-struct StatusBarView: View {
+struct HomeStatusBarView: View {
     var homeViewModel: HomeViewModel
     var body: some View {
         HStack(spacing: 0) {
@@ -58,7 +59,7 @@ struct StatusBarView: View {
 }
 
 
-struct CardGuestView: View {
+struct HomeCardGuestView: View {
     var homeViewModel: HomeViewModel
     var body: some View {
         VStack(spacing: 0) {
@@ -96,7 +97,7 @@ struct CardGuestView: View {
                     Spacer()
                 }
             }.frame(height: 70)
-                .padding(.bottom, 21)
+                .padding(.bottom, 20)
             Button {
                 // 작성하기
             } label: {
@@ -117,7 +118,260 @@ struct CardGuestView: View {
             RoundedRectangle(cornerRadius: 32)
                 .stroke(Color.line200, lineWidth: 1)
         )
+        .padding(.horizontal, 20)
     }
+}
+
+struct HomeSelectionTabView: View {
+    var homeViewModel: HomeViewModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("기업")
+                .korFont(.H4KrBd)
+                .foregroundStyle(homeViewModel.companyClubSelection == .company ? Color.textPrimary : Color.textDisabled)
+                .padding(.leading, 24)
+                .onTapGesture {
+                    withAnimation {
+                        homeViewModel.companyClubSelection = .company
+                    }
+                }
+            Text("동아리")
+                .korFont(.H4KrBd)
+                .foregroundStyle(homeViewModel.companyClubSelection == .club ? Color.textPrimary : Color.textDisabled)
+                .padding(.leading, 16)
+                .onTapGesture {
+                    withAnimation {
+                        homeViewModel.companyClubSelection = .club
+                    }
+                }
+            Spacer()
+        }.padding(.top, 38)
+            .padding(.bottom, 14)
+    }
+}
+
+struct HomeCategorySelectionCell: View {
+    var homeViewModel: HomeViewModel
+    let title: String
+    let category: HomeCompanyClubSelection
+    let companySelection: HomeCompanyCategory?
+    let clubSelection: HomeClubCategory?
+    var isCompanyMatched: Bool {
+        homeViewModel.homeCompanyCategory == companySelection
+    }
+    var isClubMatched: Bool {
+        homeViewModel.homeClubCategory == clubSelection
+    }
+    var body: some View {
+        if companySelection != nil {
+            Text(title)
+                .korFont(.B1KrMd)
+                .foregroundStyle(
+                    isCompanyMatched ? Color.textBrand : Color.textDisabled
+                )
+                .padding(.vertical, 6)
+                .padding(.horizontal, 10)
+                .clipShape(RoundedRectangle(cornerRadius: 999))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 999)
+                        .stroke(isCompanyMatched ? Color.textBrand : Color.line200, lineWidth: 1)
+                ).padding(.trailing, 4)
+        }
+        else {
+            Text(title)
+                .korFont(.B1KrMd)
+                .foregroundStyle(
+                    isClubMatched ? Color.textBrand : Color.textDisabled
+                )
+                .padding(.vertical, 6)
+                .padding(.horizontal, 10)
+                .clipShape(RoundedRectangle(cornerRadius: 999))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 999)
+                        .stroke(isClubMatched ? Color.textBrand : Color.line200, lineWidth: 1)
+                ).padding(.trailing, 4)
+        }
+    }
+}
+
+struct HomeCategorySelectionTab: View {
+    var homeViewModel: HomeViewModel
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+                if homeViewModel.companyClubSelection == .company {
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "전체", category: .company, companySelection: .all, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .all
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "식품", category: .company, companySelection: .food, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .food
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "건강", category: .company, companySelection: .health, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .health
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "생활", category: .company, companySelection: .lifestyle, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .lifestyle
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "교육", category: .company, companySelection: .education, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .education
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "뷰티", category: .company, companySelection: .beauty, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .beauty
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "기타", category: .company, companySelection: .others, clubSelection: nil)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeCompanyCategory = .others
+                            }
+                        }
+                }
+                else {
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "전체", category: .club, companySelection: nil, clubSelection: .all)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .all
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "기획/아이디어", category: .club, companySelection: nil, clubSelection: .planningAndIdeas)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .planningAndIdeas
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "광고/마케팅", category: .club, companySelection: nil, clubSelection: .advertisingAndMarketing)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .advertisingAndMarketing
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "디자인", category: .club, companySelection: nil, clubSelection: .design)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .design
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "사진/영상", category: .club, companySelection: nil, clubSelection: .photographyAndVideo)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .photographyAndVideo
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "IT/소프트웨어", category: .club, companySelection: nil, clubSelection: .iTAndSoftware)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .iTAndSoftware
+                            }
+                        }
+                    HomeCategorySelectionCell(homeViewModel: homeViewModel, title: "기타", category: .club, companySelection: nil, clubSelection: .others)
+                        .onTapGesture {
+                            withAnimation {
+                                homeViewModel.homeClubCategory = .others
+                            }
+                        }
+                }
+            }.padding(.leading, 20)
+                .padding(.vertical, 1)
+        }.scrollIndicators(.hidden)
+    }
+}
+
+struct HomeListCell: View {
+    var homeListCellViewModel: HomeListCellViewModel
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            ZStack {
+                Image(.rectangle1363)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        Spacer()
+                        Image(.icBookmark)
+                            .renderingMode(.template)
+                            .padding(.top, 12)
+                            .padding(.trailing, 12)
+                            .foregroundStyle(
+                                homeListCellViewModel.isBookmarked ? Color.textBrand :  Color.textSecondary
+                            )
+                            .onTapGesture {
+                                withAnimation {
+                                    homeListCellViewModel.isBookmarked.toggle()
+                                }
+                            }
+                    }
+                    Spacer()
+                }
+            }
+            HStack(spacing: 0) {
+                Text(homeListCellViewModel.companyName)
+                    .korFont(.T4KrBd)
+                    .foregroundStyle(Color.textPrimary)
+                Spacer()
+            }.padding(.top, 12)
+                .padding(.leading, 20)
+            
+            HStack(spacing: 0) {
+                Text("제안하기")
+                    .korFont(.B2KrMd)
+                    .foregroundStyle(Color.textSecondary)
+                Image(.icRight)
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .foregroundStyle(Color.textSecondary)
+                Spacer()
+            }.padding(.top, 7)
+                .padding(.bottom, 16)
+                .padding(.leading, 20)
+        }.background(Color.bgWhite)
+            .clipShape(RoundedRectangle(cornerRadius: 24))
+            .overlay(
+                RoundedRectangle(cornerRadius: 24)
+                    .stroke(Color.line200, lineWidth: 1)
+            )
+    }
+}
+
+struct HomeListView: View {
+    var homeViewModel: HomeViewModel
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 15),
+        GridItem(.flexible()),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(1..<21) { index in
+                    HomeListCell(homeListCellViewModel: HomeListCellViewModel(companyName: "무신사", imageURL: ""))
+                }
+            }
+        }.padding(.top, 15)
+            .padding(.horizontal, 20)
+            .scrollIndicators(.hidden)
+    }
+    
 }
 
 
--- Spon-Us/Home/HomeViewModel.swift
+++ Spon-Us/Home/HomeViewModel.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
+enum HomeCompanyClubSelection {
+    case club, company
+}
+
+enum HomeCompanyCategory {
+    case all, food, health, lifestyle, education, beauty, others
+}
+
+enum HomeClubCategory {
+    case all, planningAndIdeas, advertisingAndMarketing, design, photographyAndVideo, iTAndSoftware, others
+}
+
 @Observable
 final class HomeViewModel {
     var isLoaded = false
     var unreadNotificationsExist = false
     var isPortfolioUploaded = true
+    var companyClubSelection = HomeCompanyClubSelection.company
+    var homeCompanyCategory = HomeCompanyCategory.all
+    var homeClubCategory = HomeClubCategory.all
 }
