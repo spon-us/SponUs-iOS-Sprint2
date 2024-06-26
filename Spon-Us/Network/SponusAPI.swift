fec03e41571698797b0a11109645d6279c927515
00yhsp <00yhsp@naver.com>
2024-06-27 00:45:40 +0900

    더미 액세스토큰 제거

--- Spon-Us/Network/SponusAPI.swift
+++ Spon-Us/Network/SponusAPI.swift
@@ -76,7 +76,7 @@ extension SponusAPI: TargetType {
     var headers: [String : String]? {
         
         // TODO: 로그인 API 달리면 토큰 바꾸기
-        let auth = ["Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiZW1haWwiOiJzdHJpbmciLCJhdXRoIjoiQ0xVQiIsImlhdCI6MTcxOTQwMDk3NywiZXhwIjoxNzIwNDAwOTc3fQ.kTrXPR2ti5EAXKTEH9sbygESfRBIipe_jn3AYCO2Vzk"]
+        let auth = ["Authorization": ""]
         
         switch self {
         case .getOrganizations:
