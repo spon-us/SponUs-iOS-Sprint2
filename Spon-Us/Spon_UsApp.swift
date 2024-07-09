797099d1a81c0a6c1e586a0c5f73fbecab5784c5
kimsoomin <skno1382@soongsil.ac.kr>
2024-07-10 01:50:50 +0900

    [#42] 머지 병합

--- Spon-Us/Spon_UsApp.swift
+++ Spon-Us/Spon_UsApp.swift
@@ -15,7 +15,7 @@ struct Spon_UsApp: App {
     
     var body: some Scene {
         WindowGroup() {
-            if (vm.loginSuccess || TokenManager.shared.isAutoLogin ?? false) && !TokenManager.shared.isRefreshTokenExpired() {
+            if (vm.loginSuccess || (TokenManager.shared.isAutoLogin ?? false)) && !TokenManager.shared.isRefreshTokenExpired() {
                 HomeView()
             } else {
                 OnBoardingView()
