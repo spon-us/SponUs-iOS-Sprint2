5a05418e52cd48fbb547f3dd2e694270b26b1f1f
YuBin Kim <yubin412@naver.com>
2024-08-17 21:48:37 +0900

    [#74] BackSwipeGestureExtension 추가

---
+++ Spon-Us/Extention/BackSwipeGestureExtension.swift
@@ -0,0 +1,19 @@
+//
+//  BackSwipeGestureExtension.swift
+//  Spon-Us
+//
+//  Created by KimYuBin on 8/17/24.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
