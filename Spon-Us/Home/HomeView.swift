d8ae039a385cc55312de6988b89c7d8fbf81bb7e
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2024-09-05 21:56:04 +0900

    [#87] 홈뷰 포트폴리오 작성 카드 조건 추가 / 프로필이미지 및 단체명 추가 (#94)
    * [#87] 홈뷰 포트폴리오 작성 카드 조건 추가
    
    * [#87] 홈뷰 포트폴리오 작성 카드 프로필이미지/단체명 추가

--- Spon-Us/Home/HomeView.swift
+++ Spon-Us/Home/HomeView.swift
@@ -17,16 +17,21 @@ struct HomeView: View {
                 Text("로그아웃 Test 버튼!!")
             }
             HomeStatusBarView(homeViewModel: homeViewModel)
-            if homeViewModel.isPortfolioUploaded {
+
+            if !homeViewModel.isPortfolioExist {
                 HomeCardGuestView(homeViewModel: homeViewModel)
             }
+
             HomeSelectionTabView(homeViewModel: homeViewModel)
 
             HomeCategorySelectionTab(homeViewModel: homeViewModel)
 
             HomeListView(homeViewModel: homeViewModel)
         }.background(Color.bgSecondary)
-            .onAppear(perform: homeViewModel.onHomeViewAppear)
+            .onAppear {
+                homeViewModel.onHomeViewAppear()
+                homeViewModel.verifyPortfolioExist()
+            }
             .navigationDestination(isPresented: $homeViewModel.goToCompanyProfileView) {
                 CompanyProfileView(
                     companyProfileViewModel: CompanyProfileViewModel(
@@ -90,14 +95,22 @@ struct HomeCardGuestView: View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
                 VStack(spacing: 0) {
-                    AsyncImage(url: URL(string: "https://www.example.com"))
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
+                    AsyncImage(url: URL(string: homeViewModel.myImageUrlStr)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                    } placeholder: {
+                        Image(systemName: "person")
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                    }
                     Spacer()
                 }.padding(.trailing, 16)
                 VStack(spacing: 0) {
                     HStack(spacing: 0) {
-                        Text("Guest님")
+                        Text("\(homeViewModel.myOrgName)님")
                             .korFont(.T3KrBd)
                         Spacer()
                     }.padding(.bottom, 4)
--- Spon-Us/Home/HomeViewModel.swift
+++ Spon-Us/Home/HomeViewModel.swift
@@ -40,6 +40,11 @@ final class HomeViewModel {
     var page: Int = 1
     let size: Int = 20
 
+    var myOrgId: Int?
+    var myImageUrlStr: String = ""
+    var myOrgType: CompanyClubSelection = .company
+    var myOrgName: String = "Guest"
+
     var selectedCompany: CompanyModel = .init(
         id: 0,
         name: "",
@@ -82,7 +87,6 @@ final class HomeViewModel {
     var topID: Int = -1
     var lastID: Int = -1
 
-    var isPortfolioUploaded = true
     var companyClubSelection = CompanyClubSelection.company
     var companyCategory = CompanyCategory.all
     var clubCategory = ClubCategory.all
@@ -90,7 +94,9 @@ final class HomeViewModel {
     var goToClubProfileView = false
     
     var currentBookmarkStatus = false
-    
+
+    var isPortfolioExist = false
+
     func fetchOrganizations(type: CompanyClubSelection, completion: @escaping (Bool) -> Void) {
         provider.request(.getOrganizations(organizationType: type.rawValue, page: page, size: size)) {[weak self] response in
             switch response {
@@ -351,5 +357,50 @@ final class HomeViewModel {
         }
     }
 
+    func getMyOrgIdAndImgUrlAndTypeAndName(completion: @escaping (Bool) -> Void) {
+        provider.request(.getMyOrganization) { [weak self] result in
+            switch result {
+            case .success(let response):
+                guard let myOrgResp = try? response.map(MyOrganizationTypeResponse.self)
+                else {
+                    completion(false)
+                    return
+                }
+                self?.myOrgId = myOrgResp.content.id
+                self?.myImageUrlStr = myOrgResp.content.imageUrl ?? ""
+                self?.myOrgName = myOrgResp.content.name
+                if myOrgResp.content.organizationType == "COMPANY" {
+                    self?.myOrgType = .company
+                    completion(false)
+                }
+                else {
+                    self?.myOrgType = .club
+                    completion(true)
+                }
+            case .failure:
+                debugPrint("getMyOrgId 네트워크 요청 실패🚨")
+                completion(false)
+            }
+        }
+    }
+
+    func verifyPortfolioExist() {
+        getMyOrgIdAndImgUrlAndTypeAndName { [weak self] success in
+            if success {
+                guard let myId = self?.myOrgId else { return }
+                self?.provider.request(.getPortfolios(page: 0, size: 1, clubId: myId)) { result in
+                    switch result {
+                    case .success(let response):
+                        guard let myPortfolioResponse = try? response.map(GetPortfolioResponse.self) else { return }
+                        if myPortfolioResponse.content.content.count != 0 {
+                            self?.isPortfolioExist = true
+                        }
+                    case .failure(let err):
+                        debugPrint(err.localizedDescription)
+                    }
+                }
+            }
+        }
+    }
 
 }
